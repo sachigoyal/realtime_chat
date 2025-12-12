3ae4e74a872f2ccbcf7bdac635d5b194cee88ea7
sachigoyal <sachigoyal27@gmail.com>
2025-12-12 12:37:22 +0530

    fix

--- src/proxy.ts
+++ src/proxy.ts
@@ -1,7 +1,7 @@
 import { NextRequest, NextResponse } from "next/server"
 import { redis } from "@/lib/redis"
 import { nanoid } from "nanoid"
-
+import { isbot } from "isbot"
 
 export const proxy = async (req: NextRequest) => {
     const pathname = req.nextUrl.pathname
@@ -17,6 +17,11 @@ export const proxy = async (req: NextRequest) => {
     }>(`meta:${roomId}`)
 
     if(!meta) return NextResponse.redirect(new URL("/?error=room-not-found", req.url))
+
+    const userAgent = req.headers.get("user-agent")
+    if (isbot(userAgent)) {
+        return NextResponse.next()
+    }
 
     const existingToken = req.cookies.get("x-auth-token")?.value
 
